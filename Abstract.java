352e30453998503ab8f6fb8ead67e9c913078703
Thamasha-ru <thamasharu@gmail.com>
2022-11-11 16:58:26 +0530

    changes

--- Abstract.java
+++ Abstract.java
@@ -13,8 +13,10 @@ abstract class Bus extends Vehicle{
         //body
     }
 }
-//abstract super class ekk thiyeddi sub class liyddi ekko sub class abstract krnn oni extend krddi,nthnm method overiding krnn oni
 
+//abstract class ekak kiynne abstract method 1k ho athi clz ekak(body ekak nthi method 1k hoo)
+//abstract super class ekk thiyeddi sub class liyddi ekko sub class abstract krnn oni extend krddi,nthnm method overiding krnn oni
+//abstract class ekaka,
 //normal method==>can
 //object hadann++>baaa
 //sub class object===>can
@@ -22,3 +24,6 @@ abstract class Bus extends Vehicle{
 
 Vehicle v = new Vehicle(); //no
 Vehicle v1 = new Car(); //yes
+
+
+//
---
+++ AbstractClass.java
@@ -0,0 +1,16 @@
+abstract class Vehicle{
+    abstract void park();
+    void method(){
+        //bhfgrjgt
+    }
+}
+abstract class Car extends Vehicle{
+
+}
+
+//abstract class ekakin inherit weddi abstract class eke method
+// e widiytm thiyagnn oninm sub class ekath abstract clz ekak krnn oni
+
+//nthnm sub class eka overide krla sub class eke method eka athult body ekak denna.
+// ethkot abstract metod ekak newine.e nisa eka abstract clss ekk newei
+
